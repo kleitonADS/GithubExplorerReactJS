34fba3de05bdebe5f4e6de751a771db77c4f12ba
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2021-07-29 16:09:30 -0400

    add files
    add styles  and some loaders , css-loader and style-loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import './Styles/global.css';
+
 export function App(){
     return <h1>Hello! ReactJS!!!</h1>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@
 import { render} from 'react-dom';
 import {App} from './App.jsx';
 
+
 render(
     <App/>,
     document.getElementById('root')
