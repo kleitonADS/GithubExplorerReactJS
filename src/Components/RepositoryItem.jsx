5383e93090b09a95617f5eeb402432ee0e286e95
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2021-08-05 11:30:07 -0400

    update project
    Learning how to use useEffect() and get datas from github

--- src/Components/RepositoryItem.jsx
+++ src/Components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props){
     return(
         <li className="item-list">
-            <strong className="title-repository">{props.repository.title}</strong>
+            <strong className="title-repository" >{props.repository.name} </strong>
             <p className="description-repository">{props.repository.description}</p>
-            <a href={props.repository.link} className="link-repository">Link</a>
+            <a href={props.repository.html_url} className="link-repository" >Link</a>
         </li>
     )
 }
--- src/Components/RepositoryList.jsx
+++ src/Components/RepositoryList.jsx
@@ -1,16 +1,27 @@
 import { RepositoryItem } from "./RepositoryItem.jsx";
+import "../Styles/repositories.scss"
+import { useEffect, useState } from "react";
 
-const repository = {
-    title: 'JS Project',
-    description: 'Project create with React JS' ,
-    link: '#'
-}
+
 
 export function RepositoryList(){
- return(
+ 
+    const [repositories, setRepositories] = useState([]);
+ 
+    useEffect(() =>{
+        fetch('https://api.github.com/users/kleitonADS/repos')
+        .then( response => response.json())
+        .then(data => setRepositories(data));
+
+    },[])
+ 
+    return(
     <section className="repository-list">
+        <h1>Repository Listing</h1>
         <ul className="list">
-            <RepositoryItem repository={repository}/>
+        { repositories.map(repository => {
+            return <RepositoryItem key={repository.name} repository={repository}/>
+        })} 
         </ul>
     </section>
  )
